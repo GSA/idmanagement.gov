46c61069c18eb23647b3587ebb06b26342ec510a
Ryan Dickson <ryan.dickson@certipath.com>
2021-02-02 11:22:40 -0500

    Update gexfjs.js

--- assets/js/gexfjs.js
+++ assets/js/gexfjs.js
@@ -920,7 +920,7 @@ $(document).ready(function() {
         var _cG = $("#leftcolumn");
         if (_cG.offset().left < 0) {
             _cG.animate({
-                "left" : "0px"
+                "left" : "-200px"
             }, function() {
                 $("#aUnfold").attr("class","leftarrow");
                 $("#zonecentre").css({
