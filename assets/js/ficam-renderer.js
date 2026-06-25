2f66d94d2562f6df7b907a8969e65ddf51900459
Clayton J Barnette <110616975+claytonjbarnette@users.noreply.github.com>
2026-06-25 08:28:33 -0400

    updated Standard heading to Policies & Standards

--- assets/js/ficam-renderer.js
+++ assets/js/ficam-renderer.js
@@ -347,7 +347,7 @@ class FicamSectionRenderer {
     });
 
     // References
-    body.appendChild(this.createRowLabel('References'));
+    body.appendChild(this.createRowLabel('Policies & Standards'));
     const referencesRow = document.createElement('div');
     referencesRow.className = 'tags-row';
     referencesRow.style.display = 'flex';
