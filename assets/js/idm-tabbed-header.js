9cde507bb0f6e39ac6e5d0e7426703cb27e33987
Clayton Barnette <clayton.barnette@gsa.gov>
2026-08-19 19:23:18 -0400

    updated navigation behavior, tab durations, and navigation look and feel

--- assets/js/idm-tabbed-header.js
+++ assets/js/idm-tabbed-header.js
@@ -29,10 +29,11 @@
   function TabbedHeader(root) {
     this.root = root;
     this.tabs = Array.prototype.slice.call(root.querySelectorAll('[role="tab"]'));
+    this.tabLabels = Array.prototype.slice.call(root.querySelectorAll(".idm-tabbed-header__tab-label"));
     this.panels = Array.prototype.slice.call(root.querySelectorAll('[role="tabpanel"]'));
     this.toggle = root.querySelector("[data-idm-autoplay-toggle]");
     this.toggleLabel = root.querySelector("[data-idm-autoplay-label]");
-    this.defaultInterval = validInterval(root.dataset.defaultInterval, 10);
+    this.defaultInterval = validInterval(root.dataset.defaultInterval, 15);
     this.pauseOnHover = asBoolean(root.dataset.pauseOnHover, true);
     this.pauseOnFocus = asBoolean(root.dataset.pauseOnFocus, true);
     this.stopAfterManual = asBoolean(root.dataset.stopAfterManual, true);
@@ -43,6 +44,8 @@
     this.pauseReasons = new Set();
     this.activeIndex = 0;
     this.timer = null;
+    this.deadline = 0;
+    this.remainingMs = null;
     this.resizeFrame = null;
   }
 
@@ -52,6 +55,7 @@
 
     this.root.classList.add("is-enhanced");
     this.syncPanelHeight();
+    this.syncTabLabelTooltips();
     this.tabs.forEach(function (tab, index) {
       tab.addEventListener("click", function () {
         self.activate(index, { focus: false, manual: true });
@@ -62,6 +66,7 @@
     });
 
     this.toggle.addEventListener("click", function () {
+      if (self.autoplayEnabled) self.captureRemainingTime();
       self.autoplayEnabled = !self.autoplayEnabled;
       self.pauseReasons.delete("manual");
       self.updateToggle();
@@ -89,12 +94,14 @@
       window.cancelAnimationFrame(self.resizeFrame);
       self.resizeFrame = window.requestAnimationFrame(function () {
         self.syncPanelHeight();
+        self.syncTabLabelTooltips();
       });
     });
 
     if (document.fonts && document.fonts.ready) {
       document.fonts.ready.then(function () {
         self.syncPanelHeight();
+        self.syncTabLabelTooltips();
       });
     }
 
@@ -114,7 +121,6 @@
 
     this.activate(0, { focus: false, manual: false });
     this.updateToggle();
-    this.schedule();
   };
 
   TabbedHeader.prototype.syncPanelHeight = function () {
@@ -137,6 +143,35 @@
     }
   };
 
+  TabbedHeader.prototype.removeTabLabelTooltip = function (tab) {
+    var tooltip = tab.querySelector(".idm-tabbed-header__label-tooltip");
+    if (tooltip) tooltip.remove();
+    tab.removeAttribute("aria-describedby");
+  };
+
+  TabbedHeader.prototype.syncTabLabelTooltips = function () {
+    var self = this;
+    this.tabLabels.forEach(function (label) {
+      var tab = label.closest('[role="tab"]');
+      if (!tab) return;
+      var isTruncated = label.scrollWidth > label.clientWidth + 1;
+      if (isTruncated) {
+        if (!tab.querySelector(".idm-tabbed-header__label-tooltip")) {
+          var tooltip = document.createElement("span");
+          var tooltipId = tab.id + "-label-tooltip";
+          tooltip.id = tooltipId;
+          tooltip.className = "usa-tooltip__body idm-tabbed-header__label-tooltip";
+          tooltip.setAttribute("role", "tooltip");
+          tooltip.textContent = label.dataset.fullLabel;
+          tab.appendChild(tooltip);
+          tab.setAttribute("aria-describedby", tooltipId);
+        }
+      } else {
+        self.removeTabLabelTooltip(tab);
+      }
+    });
+  };
+
   TabbedHeader.prototype.activate = function (index, options) {
     var self = this;
     this.activeIndex = index;
@@ -155,6 +190,10 @@
     if (backgroundImage) {
       this.root.style.setProperty("--idm-hero-background-image", 'url("' + backgroundImage.replace(/["\\]/g, "") + '")');
     }
+
+    var seconds = validInterval(this.tabs[index].dataset.intervalSeconds, this.defaultInterval);
+    this.remainingMs = seconds * 1000;
+    this.tabs[index].style.setProperty("--idm-tab-duration", seconds + "s");
 
     if (options.focus) this.tabs[index].focus();
     if (options.manual && this.stopAfterManual) {
@@ -177,8 +216,10 @@
   };
 
   TabbedHeader.prototype.pause = function (reason) {
+    this.captureRemainingTime();
     this.pauseReasons.add(reason);
     this.clearTimer();
+    this.updateProgressState();
   };
 
   TabbedHeader.prototype.resume = function (reason) {
@@ -189,26 +230,43 @@
   TabbedHeader.prototype.clearTimer = function () {
     window.clearTimeout(this.timer);
     this.timer = null;
+    this.deadline = 0;
+  };
+
+  TabbedHeader.prototype.captureRemainingTime = function () {
+    if (this.timer && this.deadline) {
+      this.remainingMs = Math.max(0, this.deadline - performance.now());
+    }
   };
 
   TabbedHeader.prototype.schedule = function () {
     var self = this;
     this.clearTimer();
+    this.updateProgressState();
     if (!this.autoplayEnabled || this.pauseReasons.size > 0 || this.tabs.length < 2) return;
-    var seconds = validInterval(this.tabs[this.activeIndex].dataset.intervalSeconds, this.defaultInterval);
+    if (this.remainingMs === null) {
+      this.remainingMs = validInterval(this.tabs[this.activeIndex].dataset.intervalSeconds, this.defaultInterval) * 1000;
+    }
+    this.deadline = performance.now() + this.remainingMs;
     this.timer = window.setTimeout(function () {
+      self.remainingMs = null;
       self.activate((self.activeIndex + 1) % self.tabs.length, { focus: false, manual: false });
-    }, seconds * 1000);
+    }, this.remainingMs);
+  };
+
+  TabbedHeader.prototype.updateProgressState = function () {
+    this.root.classList.toggle("is-progress-paused", !this.autoplayEnabled || this.pauseReasons.size > 0);
   };
 
   TabbedHeader.prototype.updateToggle = function () {
     var isOn = this.autoplayEnabled;
-    var actionLabel = isOn ? "Stop" : "Start";
+    var actionLabel = isOn ? "Pause" : "Start";
     this.toggle.setAttribute("aria-pressed", String(isOn));
     this.toggle.setAttribute("aria-label", actionLabel);
     this.toggle.setAttribute("data-tooltip", actionLabel);
     this.toggleLabel.textContent = actionLabel;
     this.root.classList.toggle("is-autoplaying", isOn);
+    this.updateProgressState();
   };
 
   function init() {
